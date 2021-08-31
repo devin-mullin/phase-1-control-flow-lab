b572aeeb2237f175a1151ddcffb32e9c352ae67c
Devin Mullin <devin.mullin90@gmail.com>
2021-08-31 11:27:34 -0500

    commit tree ebe0f96da903babab81ae9dc6f095b5c8597740b

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if (ride <= 400) {
+    return 'This one is on me!'
+  } else if (ride > 2500) {
+    return 'No can do.'
+  } else if (ride > 2000) {
+    return 'I will gladly take your thirty bucks.'
+  } else {
+    return 'This one is on me!'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(cityName){
+  return (cityName === 'NYC' ?  'Ok, sounds good.' : 'No go.')
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  let response;
+  
+    switch(tip){
+      case 'generous':
+        return response = 'Thank you so much.'
+        break;
+      case 'not as generous':
+        return response = 'Thank you.'
+        break;
+      default: 
+        return response = 'Bye.'
+    }
+  }
